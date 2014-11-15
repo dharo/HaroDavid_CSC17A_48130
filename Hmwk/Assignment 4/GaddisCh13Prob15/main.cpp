af65e0d670b86c06295f8da784afbe3390b885fc
dharo <david.haro@me.com>
2014-11-15 01:35:09 -0800

    Assignment 4 Prob 5
    Updated class for register

--- Hmwk/Assignment 4/GaddisCh13Prob15/main.cpp
+++ Hmwk/Assignment 4/GaddisCh13Prob15/main.cpp
@@ -9,3 +9,17 @@ int main (int argc, const char * argv[]) {
     cout << "Hello, World!\n";
     return 0;
 }
+
+class CashRegister{
+	private:
+		int amount;
+		float cost;
+		const float profit=3.0e-1;
+		const float sales_tax=6.0e-2;
+	public:
+		void setCost();		//getCost(); from InventoryItem class
+		void setAmount();	//getQuantity(); from Inventory Item class
+		float addTax(float);	//get subtotal and add 6% sales tax
+		float addProfit(float);	//get item cost and add 30% profit
+		void displayRegister(); //display purchase subtotal, tax, and total		
+};
